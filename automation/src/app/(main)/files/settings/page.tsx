8878a26a95625e4cb5125014c298f62b6781dd0a
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2025-07-22 00:54:00 +0500

    add prisma setup

--- automation/src/app/(main)/files/settings/page.tsx
+++ automation/src/app/(main)/files/settings/page.tsx
@@ -1,12 +1,55 @@
-import React from "react";
+'use client'
 
+import React, { useEffect, useState } from 'react'
+import ProfileForm from '../../../components/forms/ProfileForms'
 
-export default function Settings(){
-    return (
-        <div className="flex flex-col gap-4 relative ml-20">
-            <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 bacdrop-blur-lg flex items-center border-b">
-                Settings
-            </h1>
-        </div>
-    )
+// Simulated fetch user
+const fetchUser = async () => {
+  return new Promise<{ name: string; email: string }>((resolve) =>
+    setTimeout(() => resolve({ name: 'John Doe', email: 'john@example.com' }), 1000)
+  )
 }
+
+const ProfilePage = () => {
+  const [user, setUser] = useState<{ name: string; email: string } | null>(null)
+
+  useEffect(() => {
+    fetchUser().then(setUser)
+  }, [])
+
+  const handleUpdate = async (name: string) => {
+    // your update logic here
+    console.log('Updated name:', name)
+  }
+
+  if (!user) {
+    return <div className="p-4">Loading user profile...</div>
+  }
+
+  return (
+     <div className=" ml-[83px] flex flex-col gap-4">
+      <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
+        <span>Settings</span>
+      </h1>
+      <div className="flex flex-col gap-10 p-6">
+        <div>
+          <h2 className="text-2xl font-bold">User Profile</h2>
+          <p className="text-base text-white/50">
+            Add or update your information
+          </p>
+        </div>
+        {/* <ProfilePicture
+          onDelete={removeProfileImage}
+          userImage={user?.profileImage || ''}
+          onUpload={uploadProfileImage}
+        /> */}
+        <ProfileForm
+          user={user}
+          onUpdate={handleUpdate}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ProfilePage
--- automation/src/lib/types.tsx
+++ automation/src/lib/types.tsx
@@ -1,6 +1,12 @@
 
+import z from "zod";
 import { ConnectionProviderProps } from "../providers/connections-providers";
-export type ConnectionTypes = 'Google Drive' | 'Notion' | 'Slack' | 'Discord';
+export type ConnectionTypes = 'Google Drive' | 'Notion' | 'Slack' | 'Discord' | 'LinkedIn' | 'Whatsapp' | 'Twitter' | 'Facebook' | 'Instagram' | 'GitHub';
+
+export const EditUserProfileSchema = z.object({
+  email: z.email('Required'),
+  name: z.string().min(1, 'Required'),
+})
 
 export type Connection = {
   title: ConnectionTypes
